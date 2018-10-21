bf7e690a55be1fedba6a84dc0bb3d4e931fc5c7a
Ivan Palomares <shades1017@gmail.com>
2018-10-20 21:03:52 -0500

    file modified

--- main.r
+++ main.r
@@ -8,14 +8,47 @@ test_indexes <- c(1, 2, 4, 5, 6, 7, 9, 11)
 train_data <- train_data[, train_indexes]
 test_data <- test_data[, test_indexes]
 
-NA2mean <- function(x) replace(x, is.na(x), mean(x, na.rm = TRUE))
-train_data$Age <- replace(train_data$Age, TRUE, lapply(train_data$Age, NA2mean))
-# test_data[,4] <- replace(test_data[,4], TRUE, lapply(test_data[,4], NA2mean))
-# train_data[,5] <- lapply(train_data[,5], NA2mean)
-# test_data[] <- lapply(test_data, NA2mean)
+# Survived
+train_data$Survived <- factor(train_data$Survived)
 
-# classifier <- naiveBayes(train_data[, 3:9], train_data$Survived)
-# test_pred <- predict(classifier, test_data[, 2:8])
-train_data
+# Pclass
+train_data$Pclass <- factor(train_data$Pclass, 
+                            labels=c(1, 2, 3), ordered = TRUE)
+test_data$Pclass <- factor(test_data$Pclass, 
+                           labels=c(1, 2, 3), ordered = TRUE)
+
+# Sex
+train_data$Sex <- factor(train_data$Sex, 
+                         labels=c('female', 'male'))
+test_data$Sex <- factor(test_data$Sex, 
+                        labels=c('female', 'male'))
+
+# Age
+NA2mean <- function(x) replace(x, is.na(x), mean(x, na.rm = TRUE))
+# train_data[5] <- replace(train_data[5], TRUE, lapply(train_data[5], NA2mean))
+# test_data[4] <- replace(test_data[4], TRUE, lapply(test_data[4], NA2mean))
+train_data$Age <- lapply(train_data$Age, NA2mean)
+test_data$Age <- lapply(test_data$Age, NA2mean)
+
+# SibSp
+train_data$SibSp <- factor(train_data$SibSp)
+test_data$SibSp <- factor(test_data$SibSp)
+
+# Parch
+train_data$Parch <- factor(train_data$Parch)
+test_data$Parch <- factor(test_data$Parch)
+
+# Fare
+
+# Embarked
+train_data$Embarked <- factor(train_data$Embarked)
+test_data$Embarked <- factor(test_data$Embarked)
+
+train_data <- as.data.frame(lapply(train_data, unlist))
+test_data <- as.data.frame(lapply(test_data, unlist))
+
+classifier <- naiveBayes(train_data[3:9], train_data$Survived)
+test_pred <- predict(classifier, test_data[2:8])
+classifier
 
 
